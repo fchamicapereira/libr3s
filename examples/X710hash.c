64cc31635d20bf8ee015a9b948d594769a06bfba
Francisco Pereira <fchamicapereira@gmail.com>
2020-04-08 20:23:54 +0100

    better makefile

--- examples/X710hash.c
+++ examples/X710hash.c
@@ -44,6 +44,49 @@ void ipv4_1()
     puts("");
 }
 
+void ipv4_2()
+{
+    RSSKS_cfg_t     cfg;
+    RSSKS_out_t     o;
+    RSSKS_headers_t h;
+    RSSKS_key_t     k = {
+        0x6d, 0x5a, 0x56, 0xda, 0x25, 0x5b, 0x0e, 0xc2,
+        0x41, 0x67, 0x25, 0x3d, 0x43, 0xa3, 0x8f, 0xb0,
+        0xd0, 0xca, 0x2b, 0xcb, 0xae, 0x7b, 0x30, 0xb4,
+        0x77, 0xcb, 0x2d, 0xa3, 0x80, 0x30, 0xf2, 0x0c,
+        0x6a, 0x42, 0xb7, 0x3b, 0xbe, 0xac, 0x01, 0xfa,
+        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+        0x00, 0x00, 0x00, 0x00
+    };
+
+    cfg = RSSKS_cfg_init();
+    
+    RSSKS_cfg_load_in_opt(&cfg, RSSKS_IN_OPT_NON_FRAG_IPV4);
+
+    h.ipv4_dst[0] = 65;
+    h.ipv4_dst[1] = 69;
+    h.ipv4_dst[2] = 140;
+    h.ipv4_dst[3] = 83;
+
+    h.tcp_dst[0]  = (4739 >> 8) & 0xff;
+    h.tcp_dst[1]  = (4739 >> 0) & 0xff;
+
+    h.ipv4_src[0] = 199;
+    h.ipv4_src[1] = 92;
+    h.ipv4_src[2] = 111;
+    h.ipv4_src[3] = 2;
+
+    h.tcp_src[0]  = (14230 >> 8) & 0xff;
+    h.tcp_src[1]  = (14230 >> 0) & 0xff;
+
+    print_headers(cfg, h);
+
+    o = hash(cfg, k, h);
+
+    print_hash_output(o);
+    puts("");
+}
+
 void ipv4_tcp_1()
 {
     RSSKS_cfg_t     cfg;
@@ -106,18 +149,18 @@ void ipv6_tcp_1()
     
     RSSKS_cfg_load_in_opt(&cfg, RSSKS_IN_OPT_NON_FRAG_IPV6_TCP);
 
-    h.ipv6_dst[0] = 0x3f;
-    h.ipv6_dst[1] = 0xfe;
-    h.ipv6_dst[2] = 0x25;
-    h.ipv6_dst[3] = 0x01;
+    h.ipv6_dst[0]  = 0x3f;
+    h.ipv6_dst[1]  = 0xfe;
+    h.ipv6_dst[2]  = 0x25;
+    h.ipv6_dst[3]  = 0x01;
 
-    h.ipv6_dst[4] = 0x02;
-    h.ipv6_dst[5] = 0x00;
-    h.ipv6_dst[6] = 0x00;
-    h.ipv6_dst[7] = 0x03;
+    h.ipv6_dst[4]  = 0x02;
+    h.ipv6_dst[5]  = 0x00;
+    h.ipv6_dst[6]  = 0x00;
+    h.ipv6_dst[7]  = 0x03;
 
-    h.ipv6_dst[8] = 0;
-    h.ipv6_dst[9] = 0;
+    h.ipv6_dst[8]  = 0;
+    h.ipv6_dst[9]  = 0;
     h.ipv6_dst[10] = 0;
     h.ipv6_dst[11] = 0;
 
@@ -126,21 +169,21 @@ void ipv6_tcp_1()
     h.ipv6_dst[14] = 0;
     h.ipv6_dst[15] = 1;
 
-    h.tcp_dst[0]  = (1766 >> 8) & 0xff;
-    h.tcp_dst[1]  = (1766 >> 0) & 0xff;
+    h.tcp_dst[0]   = (1766 >> 8) & 0xff;
+    h.tcp_dst[1]   = (1766 >> 0) & 0xff;
 
-    h.ipv6_src[0] = 0x3f;
-    h.ipv6_src[1] = 0xfe;
-    h.ipv6_src[2] = 0x25;
-    h.ipv6_src[3] = 0x01;
+    h.ipv6_src[0]  = 0x3f;
+    h.ipv6_src[1]  = 0xfe;
+    h.ipv6_src[2]  = 0x25;
+    h.ipv6_src[3]  = 0x01;
 
-    h.ipv6_src[4] = 0x02;
-    h.ipv6_src[5] = 0x00;
-    h.ipv6_src[6] = 0x1f;
-    h.ipv6_src[7] = 0xff;
+    h.ipv6_src[4]  = 0x02;
+    h.ipv6_src[5]  = 0x00;
+    h.ipv6_src[6]  = 0x1f;
+    h.ipv6_src[7]  = 0xff;
 
-    h.ipv6_src[8] = 0;
-    h.ipv6_src[9] = 0;
+    h.ipv6_src[8]  = 0;
+    h.ipv6_src[9]  = 0;
     h.ipv6_src[10] = 0;
     h.ipv6_src[11] = 0;
 
@@ -149,8 +192,8 @@ void ipv6_tcp_1()
     h.ipv6_src[14] = 0;
     h.ipv6_src[15] = 7;
 
-    h.tcp_src[0]  = (2794 >> 8) & 0xff;
-    h.tcp_src[1]  = (2794 >> 0) & 0xff;
+    h.tcp_src[0]   = (2794 >> 8) & 0xff;
+    h.tcp_src[1]   = (2794 >> 0) & 0xff;
 
     print_headers(cfg, h);
 
@@ -162,9 +205,7 @@ void ipv6_tcp_1()
 
 int main () {
     ipv4_1();
-
+    ipv4_2();
     ipv4_tcp_1();
-
     ipv6_tcp_1();
-    
 }
--- makefile
+++ makefile
@@ -1,6 +1,4 @@
 CC=g++ -Wall -fPIC
-Z3_DIR=$(VIGOR_DIR)/z3
-LIB_FLAGS=-lz3 -Wl,-rpath,$(Z3_DIR)/lib -L$(Z3_DIR)/lib -I$(Z3_DIR)/build/include
 
 DIR=$(shell pwd)
 SRC=$(DIR)/src
@@ -8,9 +6,11 @@ BUILD=$(DIR)/build
 BIN=$(DIR)/bin
 EX=$(DIR)/examples
 
+Z3_DIR=$(VIGOR_DIR)/z3
+LIB_FLAGS=-lz3 -Wl,-rpath,$(Z3_DIR)/lib -L$(Z3_DIR)/lib -I$(Z3_DIR)/build/include
+
 LIB_RSSKS=$(BUILD)/lib
 LIB_RSSKS_INCLUDE=$(BUILD)/include
-
 LIB_RSSKS_FLAGS=-lrssks -Wl,-rpath,$(LIB_RSSKS) -L$(LIB_RSSKS) -I$(LIB_RSSKS_INCLUDE)
 
 all: build
@@ -18,29 +18,32 @@ all: build
 debug: CC += -D DEBUG
 debug: build
 
-build: hash.o util.o solver.o
+build:
+$(LIB_RSSKS)/librssks.so: $(BUILD)/hash.o $(BUILD)/util.o $(BUILD)/solver.o
 	@mkdir -p $(BUILD) $(LIB_RSSKS) $(LIB_RSSKS_INCLUDE)
 	$(CC) -shared -o $(LIB_RSSKS)/librssks.so $(BUILD)/solver.o $(BUILD)/hash.o $(BUILD)/util.o $(LIB_FLAGS)
 	@cp $(SRC)/rssks.h $(LIB_RSSKS_INCLUDE)/rssks.h
 
 examples: $(LIB_RSSKS)/librssks.so
-	@mkdir -p $(BIN)
-	$(CC) $(EX)/X710hash.c -o $(BIN)/X710hash $(LIB_FLAGS) $(LIB_RSSKS_FLAGS)
-	$(CC) $(EX)/symmetric_rss.c -o $(BIN)/symmetric_rss $(LIB_FLAGS) $(LIB_RSSKS_FLAGS)
+	$(MAKE) -C $(EX)
 
-solver.o: $(SRC)/solver.c $(SRC)/solver.h $(SRC)/util.h $(SRC)/hash.h $(SRC)/rssks.h
+solver:
+$(BUILD)/solver.o: $(SRC)/solver.c $(SRC)/solver.h $(SRC)/util.h $(SRC)/hash.h $(SRC)/rssks.h
 	@mkdir -p $(BUILD)
 	$(CC) -c $(SRC)/solver.c -o $(BUILD)/solver.o $(LIB_FLAGS)
 
-hash.o: $(SRC)/hash.c $(SRC)/hash.h $(SRC)/util.h $(SRC)/rssks.h
+hash:
+$(BUILD)/hash.o: $(SRC)/hash.c $(SRC)/hash.h $(SRC)/util.h $(SRC)/rssks.h
 	@mkdir -p $(BUILD)
 	$(CC) -c $(SRC)/hash.c -o $(BUILD)/hash.o $(LIB_FLAGS)
 
-util.o: $(SRC)/util.c
+util:
+$(BUILD)/util.o: $(SRC)/util.c
 	@mkdir -p $(BUILD)
 	$(CC) -c $(SRC)/util.c -o $(BUILD)/util.o
 
 clean:
 	rm -rf $(BUILD) $(BIN) *.o *~ main find_k* proof* unsat-core* check_k*
+	$(MAKE) -C $(EX) clean
 
-.PHONY: run
+.PHONY: run examples
