baafde8b1c23ab1f6ca20c16b9b9cb9f8c021df3
Francisco Pereira <fchamicapereira@gmail.com>
2020-04-13 16:52:06 +0100

    fixed examples

--- examples/two_keys_symmetric_session_2_cnstrs.c
+++ examples/two_keys_symmetric_session_2_cnstrs.c
@@ -6,11 +6,11 @@ Z3_ast k1_k2_cnstrs(RSSKS_cfg_t rssks_cfg, Z3_context ctx, Z3_ast d1, Z3_ast d2)
     Z3_ast d2_ipv4_src, d2_ipv4_dst;
     Z3_ast and_args[2];
 
-    d1_ipv4_src = RSSKS_extract_pf_from_d(rssks_cfg, ctx, d1, RSSKS_PF_IPV4_SRC);
-    d1_ipv4_dst = RSSKS_extract_pf_from_d(rssks_cfg, ctx, d1, RSSKS_PF_IPV4_DST);
+    RSSKS_extract_pf_from_d(rssks_cfg, ctx, d1, RSSKS_PF_IPV4_SRC, &d1_ipv4_src);
+    RSSKS_extract_pf_from_d(rssks_cfg, ctx, d1, RSSKS_PF_IPV4_DST, &d1_ipv4_dst);
 
-    d2_ipv4_src = RSSKS_extract_pf_from_d(rssks_cfg, ctx, d2, RSSKS_PF_IPV4_SRC);
-    d2_ipv4_dst = RSSKS_extract_pf_from_d(rssks_cfg, ctx, d2, RSSKS_PF_IPV4_DST);
+    RSSKS_extract_pf_from_d(rssks_cfg, ctx, d2, RSSKS_PF_IPV4_SRC, &d2_ipv4_src);
+    RSSKS_extract_pf_from_d(rssks_cfg, ctx, d2, RSSKS_PF_IPV4_DST, &d2_ipv4_dst);
 
     and_args[0] = Z3_mk_eq(ctx, d1_ipv4_src, d2_ipv4_dst);
     and_args[1] = Z3_mk_eq(ctx, d1_ipv4_dst, d2_ipv4_src);
@@ -24,17 +24,17 @@ Z3_ast k1_cnstrs(RSSKS_cfg_t rssks_cfg, Z3_context ctx, Z3_ast d1, Z3_ast d2)
     Z3_ast d2_ipv4_src, d2_ipv4_dst, d2_tcp_src, d2_tcp_dst;
     Z3_ast and_args[4];
 
-    d1_ipv4_src = RSSKS_extract_pf_from_d(rssks_cfg, ctx, d1, RSSKS_PF_IPV4_SRC);
-    d1_ipv4_dst = RSSKS_extract_pf_from_d(rssks_cfg, ctx, d1, RSSKS_PF_IPV4_DST);
+    RSSKS_extract_pf_from_d(rssks_cfg, ctx, d1, RSSKS_PF_IPV4_SRC, &d1_ipv4_src);
+    RSSKS_extract_pf_from_d(rssks_cfg, ctx, d1, RSSKS_PF_IPV4_DST, &d1_ipv4_dst);
 
-    d1_tcp_src  = RSSKS_extract_pf_from_d(rssks_cfg, ctx, d1, RSSKS_PF_TCP_SRC);
-    d1_tcp_dst  = RSSKS_extract_pf_from_d(rssks_cfg, ctx, d1, RSSKS_PF_TCP_DST);
+    RSSKS_extract_pf_from_d(rssks_cfg, ctx, d1, RSSKS_PF_TCP_SRC, &d1_tcp_src);
+    RSSKS_extract_pf_from_d(rssks_cfg, ctx, d1, RSSKS_PF_TCP_DST, &d1_tcp_dst);
 
-    d2_ipv4_src = RSSKS_extract_pf_from_d(rssks_cfg, ctx, d2, RSSKS_PF_IPV4_SRC);
-    d2_ipv4_dst = RSSKS_extract_pf_from_d(rssks_cfg, ctx, d2, RSSKS_PF_IPV4_DST);
+    RSSKS_extract_pf_from_d(rssks_cfg, ctx, d2, RSSKS_PF_IPV4_SRC, &d2_ipv4_src);
+    RSSKS_extract_pf_from_d(rssks_cfg, ctx, d2, RSSKS_PF_IPV4_DST, &d2_ipv4_dst);
 
-    d2_tcp_src  = RSSKS_extract_pf_from_d(rssks_cfg, ctx, d2, RSSKS_PF_TCP_SRC);
-    d2_tcp_dst  = RSSKS_extract_pf_from_d(rssks_cfg, ctx, d2, RSSKS_PF_TCP_DST);
+    RSSKS_extract_pf_from_d(rssks_cfg, ctx, d2, RSSKS_PF_TCP_SRC, &d2_tcp_src);
+    RSSKS_extract_pf_from_d(rssks_cfg, ctx, d2, RSSKS_PF_TCP_DST, &d2_tcp_dst);
 
 
     and_args[0] = Z3_mk_eq(ctx, d1_ipv4_src, d2_ipv4_dst);
