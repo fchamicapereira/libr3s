03bee20bd5cb09aa5d90f3e5a82051d7cb52ce66
Francisco Pereira <fchamicapereira@gmail.com>
2020-04-13 11:42:25 +0100

    fixed two_keys_symmetric_ip

--- examples/two_keys_symmetric_ip.c
+++ examples/two_keys_symmetric_ip.c
@@ -26,7 +26,7 @@ int validate(RSSKS_cfg_t cfg, RSSKS_key_t k1, RSSKS_key_t k2)
     for (int i = 0; i < 25; i++)
     {
         RSSKS_rand_headers(cfg, &h1);
-        h2 = RSSKS_headers_from_cnstrs(cfg, &mk_d_cnstrs, h1);
+        RSSKS_headers_from_cnstrs(cfg, h1, &mk_d_cnstrs, h2);
         RSSKS_hash(cfg, k1, h1, &o1);
         RSSKS_hash(cfg, k2, h2, &o2);
 
